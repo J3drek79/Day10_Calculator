af451ed05cc775889909ad5dc7a89866d77081c2
J3drek79 <117525013+J3drek79@users.noreply.github.com>
2022-11-18 14:33:51 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -27,19 +27,19 @@ operations = {
     "/": divide
 }
 
+
 num1 = int(input("What is the first number?: "))
 for symbol in operations:
     print(symbol)
-operation_symbol = (input("Pick an operation from the line above: "))
-num2 = int(input("What is the second number?: "))
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(n1=num1, n2=num2)
+shouldend = False
+while not shouldend:
+    operation_symbol = (input("Pick an operation: "))
+    num2 = int(input("What is the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(n1=num1, n2=num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-operation_symbol = (input("Pick another operation from the line above: "))
-num3 = int(input("What is the second number?: "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(n1=first_answer, n2=num3)
-
-print(f"{first_answer} {operation_symbol} {num2} = {second_answer}")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ") == "y":
+        num1 = answer
+    else:
+        shouldend = True
